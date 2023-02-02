39d12e36202d03f55aa79d34982a6c8ef6608d8a
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-02-02 11:35:53 +0600

    split join substring at

---
+++ split-join.js
@@ -0,0 +1,19 @@
+const lyrics = "taka o pakhi tumi uIRA uira aso. uiRa Uira aisa Tumi amar dale bose. O taka o pakhi";
+// split array dibe 
+// console.log(lyrics.split(" "));
+// console.log(lyrics.split(""));
+// console.log(lyrics.split("."));
+var lir = [
+    'taka o pakhi tumi uIRA uira aso',
+    ' uiRa Uira aisa Tumi amar dale bose',
+    ' O taka o pakhi'
+  ]
+// console.log(lir.join(" * "));
+
+console.log(lyrics.slice(7, 10));
+console.log(lyrics.substring(7, 10));
+console.log(lyrics.charAt(3));
+
+let hello = "hello";
+let world = "world!";
+console.log(hello.concat(" ", world))
